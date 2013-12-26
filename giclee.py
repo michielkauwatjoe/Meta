183ebd8bfd0e11177f4e4485d5aa6b9f1813bbc4
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2013-12-26 14:11:37 +0100

    Thought of another project name.

--- giclee.py
+++ giclee.py
@@ -11,4 +11,13 @@ class Giclee:
     """
 
     def __init__(self, size='A2', colorspace='RGB'):
+        u"""
+        Sets up a Python Imaging Library canvas.
+        """
         self.canvas = Image()
+
+    def grid(self, nx, ny, stroke='1px solid black'):
+        u"""
+        Draws an auxiliary grid. Should be useful for for example taxi cab Voronoi tesselation.
+        """
+        pass
---
+++ imftc/itsmorefuntocompute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#
+# https://github.com/michielkauwatjoe/Meta
+
+from meta.giclee import Giclee
+
+class ItsMoreFunToCompute(Giclee):
+    pass
