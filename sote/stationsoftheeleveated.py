012fd6ae43a8dd9ba109d7c45e2fe06d3b2c6b2d
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2013-12-26 13:22:42 +0100

    Setting up class for SotE print.

---
+++ sote/stationsoftheeleveated.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#
+# https://github.com/michielkauwatjoe/Meta
+
+from circus.voronoi import Voronoi
+
+class StationsOfTheElevated:
+
+    def __init__(self):
+        self.coordinates = self.loadCoordinates()
+
+    def loadCoordinates(self):
+        pass
