81014d948662fde050c21815ae013bf5ad634230
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2013-12-26 18:21:45 +0100

    Basic image loading and saving.

--- giclee.py
+++ giclee.py
@@ -11,16 +11,25 @@ class Giclee(Sizes):
     Base class for the Giclée print canvas.
     """
 
-    def __init__(self, size='A2', colorspace='RGB'):
+    def __init__(self, name='test', format='png', folder='/tmp', size='A2', colorspace='RGB', background='white'):
         u"""
         Sets up a Python Imaging Library canvas.
         """
+        self.name = name
+        self.format = format
+        self.folder = folder
+        self.colorspace = colorspace
+        self.background = background
         super(Giclee, self).__init__()
-        self.canvas = Image()
-
+        self.size_mm = self.getMmSize(size)
+        self.canvas = Image.new(colorspace, size_mm, background)
 
     def grid(self, nx, ny, stroke='1px solid black'):
         u"""
         Draws an auxiliary grid. Should be useful for for example taxi cab Voronoi tessellation.
         """
         pass
+
+    def save(self):
+        path = folder + '/', name + '.'
+        self.canvas.save(path, format)
--- sote/stationsoftheeleveated.py
+++ sote/stationsoftheeleveated.py
@@ -9,7 +9,14 @@ from circus.voronoi import Voronoi
 class StationsOfTheElevated(Giclee):
 
     def __init__(self):
+        super(Giclee, self).__init__(name='stations-of-the-elevated')
         self.coordinates = self.loadCoordinates()
+        # Place random points around it.
 
     def loadCoordinates(self):
+        # Load SVG.
         pass
+
+def main():
+    sote = StationsOfTheElevated()
+    sote.save()
