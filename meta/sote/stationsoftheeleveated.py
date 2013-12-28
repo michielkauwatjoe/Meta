0e3be87cc38bca8067e1612148d7b2339c4aba96
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2013-12-28 19:31:36 +0100

    Testing different point sets.

--- meta/sote/stationsoftheeleveated.py
+++ meta/sote/stationsoftheeleveated.py
@@ -20,8 +20,8 @@ class StationsOfTheElevated(Giclee):
 
         self.qhull = QHull()
         voronoi = self.qhull.voronoi(self.points)
-        self.drawPoints(voronoi)
         self.drawLines(voronoi)
+        self.drawPoints(voronoi)
         # Place pattern points around it in rows of several ellipses.
 
     def normalize(self, point):
@@ -29,8 +29,8 @@ class StationsOfTheElevated(Giclee):
         dy = -self.min_y
         nx = self.width / self.diff_x
         ny = self.height / self.diff_y
-        x0 = (point[0] + dx) * nx
-        y0 = (point[1] + dy) * ny
+        x0 = (point[0] + dx) * nx + (numpy.random.rand() * 3)
+        y0 = (point[1] + dy) * ny + (numpy.random.rand() * 3)
         return (x0, y0)
     
     def setMinMaxDeltas(self):
@@ -56,6 +56,7 @@ class StationsOfTheElevated(Giclee):
         points = voronoi.points
         avg = numpy.average(points, 0)
         vertices = voronoi.vertices
+        offset = 0.2
 
         for nn, vind in voronoi.ridges.items():
             (i1, i2) = sorted(vind)
@@ -73,21 +74,29 @@ class StationsOfTheElevated(Giclee):
 
             n1 = self.normalize(c1)
             n2 = self.normalize(c2)
-            self.context.set_source_rgb(0.0, 0.0, 0.0)
+
+            self.context.set_source_rgb(0.85, 0.7, 0)
+            self.context.set_line_width(0.4)
+            self.context.move_to(n1[0] + offset, n1[1] + offset)
+            self.context.line_to(n2[0] + offset, n2[1] + offset)
+            self.context.stroke()
+
+            self.context.set_source_rgb(0.8, 1, 0)
+            self.context.set_line_width(0.3)
             self.context.move_to(n1[0], n1[1])
             self.context.line_to(n2[0], n2[1])
-            self.context.set_line_width(0.4)
             self.context.stroke()
 
     def drawPoints(self, voronoi):
 
-        self.context.set_source_rgb(1, 0, 0)
+        self.context.set_source_rgb(0.8, 0.8, 0.2)
         self.context.set_line_width(0.5)
 
         for point in voronoi.points:
             n1, n2 = self.normalize(point)
-            self.context.arc(n1, n2, 2 * math.sqrt(2), -2 * math.pi, 2 * math.pi)
-            self.context.stroke()
+            self.context.arc(n1, n2, math.sqrt(2), -1 * math.pi, 1 * math.pi)
+            self.context.fill()
+            #self.context.stroke()
 
         self.context.set_source_rgb(0, 1, 0)
 
@@ -101,12 +110,26 @@ class StationsOfTheElevated(Giclee):
 
         for ridge in voronoi.ridges:
             n1, n2 = self.normalize(ridge)
-            #self.context.line_to(n1, n2)
             self.context.arc(n1, n2, 2* math.sqrt(2), -2 * math.pi, 2 * math.pi)
             self.context.stroke()
 
-
     def loadPoints(self, number_of_points, dimension):
+        return self.getCirclePoints(number_of_points)
+        #return self.getRandomPoints(number_of_points, dimension)
+
+    def getCirclePoints(self, number_of_points):
+        points = []
+        
+        for r in range(1, 20):
+            for i in range(number_of_points):
+                a = i * 360.0 / number_of_points
+                x = math.sqrt(r) * math.cos(a)
+                y = math.sqrt(r) * math.sin(a)
+                points.append((x, y))
+            number_of_points = number_of_points + 5
+        return points
+
+    def getRandomPoints(self, number_of_points, dimension):
         return numpy.random.randn(number_of_points, dimension)
 
 if __name__ == "__main__":
