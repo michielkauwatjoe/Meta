89207d7ceff9c9e8da593a33e71e1d6490531d6a
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-02-15 12:15:46 +0100

    Adding document object.

---
+++ meta/document.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# https://github.com/michielkauwatjoe/Meta
+
+
+class Document(object):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
